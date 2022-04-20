ce9bcbf5c7a2bc31093633f347f90cfc4ff91057
Javid Alizada <javid.alizada@taotesting.com>
2022-04-20 10:41:58 +0400

    first commit

---
+++ cat.py
@@ -0,0 +1,27 @@
+class Cat:
+    def __init__(self, color, cat_type, size):
+        self.size = size
+        self.color = color
+        self.cat_type = cat_type
+
+
+    def set_size(self):
+        if self.cat_type == "indoor":
+            self.size = "small"
+        else:
+            self.size = "undefined"
+
+class Tiger(Cat):
+    def __init__(self, color, cat_type, size):
+        Cat.__init__(self, color, cat_type, size)
+
+    def set_size(self):
+        if self.cat_type == "wild":
+            self.size = "big"
+        else:
+            self.size = "undefined"
+
+
+shir = Tiger(color="sari", cat_type="wild", size=12)
+
+print(shir.size)
